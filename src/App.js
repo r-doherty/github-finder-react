d5653d12fe68406c242fac28d8bdc38af0a21766
Ryan Doherty <rjd.atx@gmail.com>
2020-07-14 09:51:24 -0500

    component folder add + navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = true;
-
     return (
       <div className='App'>
-        <h1>My App</h1>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
       </div>
     );
   }
