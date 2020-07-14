9cbd4ccd28b33ee6784b524d224ad9911bf62b95
Ryan Doherty <rjd.atx@gmail.com>
2020-07-14 09:16:01 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+
+    return (
+      <div className='App'>
+        <h1>My App</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+      </div>
+    );
+  }
+}
+
+export default App;
